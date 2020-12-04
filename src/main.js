761058dabf13644efdaf0ebebac9d1df902097a5
bigbigtime <409019683@qq.com>
2020-12-04 21:56:44 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// elementui
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(ElementPlus)
   .mount("#app");
