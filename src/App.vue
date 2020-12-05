373931c20ec434a83a08f4ee0a3915c0f3b79ae6
bigbigtime <409019683@qq.com>
2020-12-05 18:04:52 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
-  <el-button type="primary">主要按钮</el-button>
 </template>
 
 <style lang="scss">
---
+++ src/views/account/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>登录页面</div>
+</template>
+
+<script>
+import { reactive, ref, onMounted, watch } from 'vue';
+export default {
+   name: "Login",
+   components: {},
+   props: {}
+}
+</script>
+<style lang='scss' scoped>
+</style>
