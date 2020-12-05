691fb215d914dc7f15f8e8d2016e02893b8f5593
bigbigtime <409019683@qq.com>
2020-12-05 20:05:56 +0800

    vue

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>登录页面</div>
+    <div id="login">
+        <div class="form-wrap">
+            <ul class="menu-tab">
+                <li class="current">登录</li>
+                <li>注册</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,4 +18,26 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
+#login {
+    height: 100vh;  // 设置高度，居于浏览器可视区高度
+    background-color: #344a5f;  // 设置背景颜色
+}
+.form-wrap {
+    width: 320px;
+    padding-top: 100px;  //上内边距 
+    margin: auto;  // 块元素水平居中
+}
+.menu-tab {
+    text-align: center;
+    li {
+        display: inline-block;
+        padding: 10px 24px;
+        margin: 0 10px;
+        color: #fff;
+        font-size: 14px;
+        border-radius: 5px;
+        cursor: pointer;
+        &.current { background-color: rgba(0, 0, 0, .1);}
+    }
+}
 </style>
