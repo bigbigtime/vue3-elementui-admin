fd3e306f3654653f45cba9fb0ee97e8a4bc514e3
bigbigtime <409019683@qq.com>
2020-12-11 23:30:31 +0800

    vue

--- vue.config.js
+++ vue.config.js
@@ -43,11 +43,11 @@ module.exports = {
     hot: true, // 开启热加载
     hotOnly: false,
     proxy: {
-      '/devApi': {
-          target: "http://www.web-jshtml.cn/api/v3", //API服务器的地址  http://www.web-jshtml.cn/api
+      [process.env.VUE_APP_API]: {
+          target: process.env.VUE_API_DEV_TARGET, //API服务器的地址  http://www.web-jshtml.cn/api/v3
           changeOrigin: true,
           pathRewrite: {
-              '^/devApi': ''
+            [`^${process.env.VUE_APP_API}`]: ''
           }
       }
     }
