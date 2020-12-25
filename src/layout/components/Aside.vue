795b44a6ddf4fa3277c60d9ffbe36930d40d1f47
bigbigtime <409019683@qq.com>
2020-12-25 21:08:42 +0800

    vue

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -1,14 +1,15 @@
 <template>
-  <el-menu default-active="2" background-color="#344a5f" text-color="#fff" active-text-color="#ffd04b">
+  <el-menu default-active="4" background-color="#344a5f" text-color="#fff" active-text-color="#ffd04b">
+    <!-- 一级菜单 -->
     <el-menu-item index="4">
       <template #title>控制台</template>
     </el-menu-item>
+    <!-- 子级菜单 -->
     <el-submenu index="1">
       <template #title>信息管理</template>
       <el-menu-item index="1-4-1">信息列表</el-menu-item>
     </el-submenu>
   </el-menu>
-
 </template>
 
 <script>
