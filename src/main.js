415758efe5d9ab2c330ca3ae45715b269389a044
bigbigtime <409019683@qq.com>
2020-12-26 16:11:04 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from "./router";
 import store from "./store";
 // elementui
 import ElementUI from "./plugins/elementui";
-// svgicon
-import SvgIcon from "@/components/svgIcon/Index.vue";
+// axios
+import axios from "axios";
 // 赋值
 const app = createApp(App);
-app
-.use(store)
-.use(router)
-.use(ElementUI)
-.component("svg-icon", SvgIcon)
-.mount("#app");
+// 全局应用配置
+app.config.globalProperties.$axios = axios;
+
+app.use(store).use(router).use(ElementUI).mount("#app");
