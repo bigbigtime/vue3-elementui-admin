3f99e8a2dd2a6e6d3e0225928699e1c015b1fbdb
yanghaimin <yanghaimin@rockminer.com>
2021-05-19 18:15:17 +0800

    vue3

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -49,7 +49,7 @@ export default {
     setup(props){
         const instance = getCurrentInstance();
         // 获取实例上下文
-        const { ctx } = getCurrentInstance();
+        const { proxy } = getCurrentInstance();
         // 用户名校验
         const validate_name_rules = (rule, value, callback) => {
             let regEmail = validate_email(value);
@@ -139,7 +139,7 @@ export default {
             const passwords = data.form.passwords;
             // 校验用户名
             if(!username || !validate_email(username)) {
-                ctx.$message.error({
+                proxy.$message.error({
                     message: "用户名不能为空 或 格式不正确",
                     type: "error"
                 })
@@ -147,7 +147,7 @@ export default {
             }
             // 校验密码
             if(!password || !validate_password(password)) {
-                ctx.$message({
+                proxy.$message({
                     message: "密码不能为空 或 格式不正确",
                     type: "error"
                 })
@@ -155,7 +155,7 @@ export default {
             }
             // 判断非 登录 时，校验两次密码
             if(data.current_menu === 'register' && (password !== passwords)) {
-                ctx.$message({
+                proxy.$message({
                     message: "两次密码不一致",
                     type: "error"
                 })
