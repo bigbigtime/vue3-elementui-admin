558251186884dc9defdad1f970a449344d3fa506
bigbigtime <409019683@qq.com>
2021-05-22 08:29:03 +0800

    vue3

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-container id="layout-container">
-        <el-aside id="layout-aside" width="250px"><layout-aside /></el-aside>
+        <el-aside id="layout-aside" :width="collapse === true ? '60px' : '250px'"><layout-aside /></el-aside>
         <el-container>
             <el-header id="layout-header" height="75px"><layout-header /></el-header>
             <el-main id="layout-main"><layout-main /></el-main>
@@ -12,19 +12,27 @@
 import LayoutAside from "./components/Aside";
 import LayoutHeader from "./components/Header";
 import LayoutMain from "./components/Main";
-
+import { useStore } from "vuex";
+import { computed } from "vue";
 export default {
    name: "Layout",
    components: { LayoutAside, LayoutHeader, LayoutMain },
    props: {},
    setup(props){
-       return {}
+       const store = useStore();
+       const collapse = computed(() => store.state.app.collapse);
+       return {
+           collapse
+       }
     }
 }
 </script>
 <style lang="scss" scpoed>
 #layout-container { height: 100vh; }
-#layout-aside { background-color: #344a5f; }
+#layout-aside { 
+    background-color: #344a5f;
+    @include webkit(transition, all .3s ease 0s);
+}
 #layout-header {
     position: relative;
     z-index: 10;
