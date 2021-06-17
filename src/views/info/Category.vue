00f233eb1127ebbd8638ed13aa7151a9d47a6d94
bigbigtime <409019683@qq.com>
2021-06-17 22:39:30 +0800

    vue3

--- src/views/info/Category.vue
+++ src/views/info/Category.vue
@@ -9,7 +9,7 @@
                         <span>{{ node.label }}</span>
                         <span>
                             <el-button size="mini" type="danger" round class="button-mini" @click="handlerCategory('child_category_add')">添加子级</el-button>
-                            <el-button size="mini" type="success" round class="button-mini" @click="handlerCategory('child_category_edit')">编辑</el-button>
+                            <el-button size="mini" type="success" round class="button-mini" @click="handlerCategory(node.level === 1 ? 'parent_category_edit' : 'child_category_edit', node)">编辑</el-button>
                             <el-button size="mini" round class="button-mini">删除</el-button>
                         </span>
                     </div>
@@ -56,48 +56,63 @@ export default {
                 children: 'children',
                 label: 'label'
             },
-            parent_category: "",   // 父级分类
-            sub_category: "",      // 子级分类
+            parent_category: "父级分类文本演示",   // 父级分类
+            sub_category: "子级分类文本演示",      // 子级分类
             button_loading: false  // 按钮加载
         })
-    const config = reactive({
-        type: "default",
-        default: {
-            title: "添加分类",        // 标题
-            parent_disabled: true,   // 父级分类禁用/启用
-            sub_disabled: true,      // 子级分类禁用/启用
-            sub_show: true           // 子级分类显示/隐藏
-        },
-        first_category_add: {
-            title: "一级分类添加",    // 标题
-            parent_disabled: false,   // 父级分类禁用/启用
-            sub_disabled: true,      // 子级分类禁用/启用
-            sub_show: false           // 子级分类显示/隐藏
-        },
-        child_category_add: {
-            title: "子级分类添加",    // 标题
-            parent_disabled: true,   // 父级分类禁用/启用
-            sub_disabled: false,      // 子级分类禁用/启用
-            sub_show: true     
-        },
-        parent_category_edit: {
-            title: "父级分类编辑",    // 标题
-            parent_disabled: false,   // 父级分类禁用/启用
-            sub_disabled: true,      // 子级分类禁用/启用
-            sub_show: false     
-        },
-        child_category_edit: {
-            title: "子级分类编辑",    // 标题
-            parent_disabled: true,   // 父级分类禁用/启用
-            sub_disabled: false,      // 子级分类禁用/启用
-            sub_show: true     
-        }
-    });
+        const config = reactive({
+            type: "default",
+            default: {
+                title: "添加分类",        // 标题
+                parent_disabled: true,   // 父级分类禁用/启用
+                sub_disabled: true,      // 子级分类禁用/启用
+                sub_show: true           // 子级分类显示/隐藏
+            },
+            first_category_add: {
+                title: "一级分类添加",    // 标题
+                parent_disabled: false,   // 父级分类禁用/启用
+                sub_disabled: true,      // 子级分类禁用/启用
+                sub_show: false,         // 子级分类显示/隐藏
+                clear: ["parent_category", "sub_category"]
+            },
+            child_category_add: {
+                title: "子级分类添加",    // 标题
+                parent_disabled: true,   // 父级分类禁用/启用
+                sub_disabled: false,      // 子级分类禁用/启用
+                sub_show: true,
+                clear: ["sub_category"],
+                create: ["parent_category"]
+            },
+            parent_category_edit: {
+                title: "父级分类编辑",    // 标题
+                parent_disabled: false,   // 父级分类禁用/启用
+                sub_disabled: true,      // 子级分类禁用/启用
+                sub_show: false     
+            },
+            child_category_edit: {
+                title: "子级分类编辑",    // 标题
+                parent_disabled: true,   // 父级分类禁用/启用
+                sub_disabled: false,      // 子级分类禁用/启用
+                sub_show: true     
+            }
+        });
         const handleNodeClick = (data) => {
             console.log(data)
         }
-        const handlerCategory = (type) => {
+        const handlerCategory = (type, node_data) => {
             config.type = type;
+            // 文本清除、还原
+            handlerInputValue();
+        }
+        const handlerInputValue = () => {
+            // 获取清除数据的对象
+            const clearObject = config[config.type].clear;
+            // 执行清除动作
+            if(clearObject && clearObject.length > 0) {
+                clearObject.forEach(item => {
+                    data[item] = "";
+                })
+            }
         }
         return {
             data,
